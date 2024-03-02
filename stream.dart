68e12f9456c6600835fdf9bab4e638a5e9bbd30d
Taksh-p <takshpatel2353@gmail.com>
2024-03-02 18:39:26 +0530

    Stream Namaste added

--- stream.dart
+++ stream.dart
@@ -1,14 +1,19 @@
-void main() async{
-  
-  await for(var number in getNumber(10)){
+void main() async {
+  await for (var number in getNumber(10)) {
     print(number);
   }
 }
 
-Stream<int> getNumber(int number) async*{
-
-  for(int i=0; i<number; i++){
+Stream<int> getNumber(int number) async* {
+  for (int i = 0; i < number; i++) {
     await Future.delayed(Duration(seconds: 1));
     yield i;
   }
+}
+
+Stream<String> getName() async* {
+  await Future.delayed(Duration(seconds: 1));
+  yield "Namaste";
+  await Future.delayed(Duration(seconds: 1));
+  yield "Duniya";
 }
